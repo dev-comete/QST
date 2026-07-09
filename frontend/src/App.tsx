7c6b68e6840aad6462f5ae94f8be9b1e77d25943
barbaraHasina <barbara.randrianarisaona@comete-group.ai>
2026-07-09 16:08:20 +0300

    add: route provider added + update of the gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { RouterProvider } from 'react-router/internal/react-server-client'
+import { router } from './other/routes/route'
 
 function App() {
 
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
