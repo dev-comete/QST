480a0c7787cd9d70ab285114a515deef88b030da
kevinMaoty-comete <kevin.rakotoandrianarivony@comete-group.ai>
2026-07-27 12:48:08 +0000

    add : temporary unable sqlite database connection in gcp shell

--- backdev/backdev/settings.py
+++ backdev/backdev/settings.py
@@ -130,6 +130,7 @@ WSGI_APPLICATION = 'backdev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -144,12 +145,13 @@ DATABASES = {
     }
 }
 
-"""DATABASES = {
+"""
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
 
