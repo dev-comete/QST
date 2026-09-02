06a6d785f58a5b55e2302ebf51230023b65b37a2
kevinMaoty-comete <kevin.rakotoandrianarivony@comete-group.ai>
2026-09-02 16:21:47 +0300

    fix: vague id in params on frontend

--- react_lms_frontend/src/api/studentQuiz.service.js
+++ react_lms_frontend/src/api/studentQuiz.service.js
@@ -13,27 +13,34 @@ export const StudentQuizService = {
   },
 
   // Démarre le quiz et récupère les questions
-  startQuiz: async (quizId) => {
-    // Remplacez par l'URL exacte de votre TakeQuizAPIView
-    const response = await apiClient.get(`quizzes/${quizId}/take/`);
-    return response.data;
-  },
-
-  // Soumet les réponses
-  submitQuiz: async (quizId, answers) => {
-    // Remplacez par l'URL exacte de votre SubmitQuizAPIView
-    const response = await apiClient.post('quizzes/student-submit/', {
-      quiz_id: quizId,
-      answers: answers
+  startQuiz: async (quizId, vagueId) => {
+    const response = await apiClient.get(`quizzes/${quizId}/take/`, {
+      params: { vague_id: vagueId } // Axios ajoute automatiquement le "?"
     });
     return response.data;
   },
 
-  // (Optionnel) Voir la correction plus tard
-  reviewQuiz: async (quizId) => {
-    const response = await apiClient.get(`quizzes/${quizId}/review/`);
+  // Soumet les réponses
+  submitQuiz: async (payload) => {
+    /* Le payload reçu ressemble à :
+      {
+        "quiz_id": 5,
+        "vague_id": 12,
+        "answers": { "1": [2], "2": [5,6] }
+      }
+    */
+    const response = await apiClient.post('quizzes/student-submit/', payload);
     return response.data;
   },
+
+  // (Optionnel) Voir la correction plus tard
+  reviewQuiz: async (quizId, vagueId) => {
+    const response = await apiClient.get(`quizzes/${quizId}/review/`, {
+      params: { vague_id: vagueId }
+    });
+    return response.data;
+  },
+  
   getBulletin: async (vagueId) => {
     // Ajustez l'URL selon votre urls.py
     const response = await apiClient.get(`quizzes/bulletin/vague/${vagueId}/`);
